a953342965dda5ad562f8f9494537e983fe45141
Evangelos Konstantinidis <93388918+vag4me@users.noreply.github.com>
2024-05-19 17:09:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.decomposition import PCA
+#from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
